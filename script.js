e1cbaed7d308eec5065e0d0e84d8e7abd6ba31cc
fukczun <arthurnobrega906@gmail.com>
2024-05-27 12:44:15 -0300

    pop up

--- script.js
+++ script.js
@@ -13,8 +13,21 @@ function getGreetingMessage() {
     }
 }
 
+function showPopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const greetingElement = document.getElementById("greeting");
     const greetingMessage = getGreetingMessage();
     greetingElement.textContent = greetingMessage;
+
+    setTimeout(showPopup, 1000)
+    
+    const closeButton = document.getElementById('closeButton');
+    closeButton.addEventListener('click', function() {
+        const popup = document.getElementById('popup');
+        popup.style.display = 'none';
+    });
 });
